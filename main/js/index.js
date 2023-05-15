1b2c6e8ac76ec9ae70cda96d14c879370e620da5
Edoardo Pizzorno <92369419+EdoardoPizzorno@users.noreply.github.com>
2023-05-15 20:50:08 +0200

    User usage progress

--- main/js/index.js
+++ main/js/index.js
@@ -5,29 +5,47 @@ window.onload = function () {
     let aProfile = $(".dropdown-item.profile").eq(0)
     let aExit = $(".dropdown-item.exit").eq(0)
 
-    let teachersSection = $("div.teachers").eq(0)
-    let studentsDefaultView = $("div.options").eq(0)
+    let studentsDefaultView = $("div.student-options").eq(0)
     let studentsMessagesSection = $("div.student-messages").eq(0)
     let studentsRegisterSection = $("div.student-register").eq(0)
     let studentsMarksSection = $("div.student-marks").eq(0)
     let studentsAbsencesSection = $("div.student-absences").eq(0)
-    let studentsInformations = $("div.informations").eq(0)
     let studentsSchoolReport = $("div.school-report").eq(0)
+    let studentsInterviewsSection = $("div.student-interviews-booking").eq(0)
+
+    let personalInformations = $("div.informations").eq(0)
+
+    let teachersDefaultView = $("div.teacher-options").eq(0)
+    let teachersMessagesSection = $("div.teacher-messages").eq(0)
+    let teachersRegisterSection = $("div.teacher-register").eq(0)
+    //let teachersMarksSection = $("div.teacher-marks").eq(0)
+    //let teachersAbsencesSection = $("div.teacher-absences").eq(0)
+    let teachersClassListSection = $("div.teacher-list").eq(0)
+    let teachersAveragesSection = $("div.teacher-averages").eq(0)
+    let teachersInterviewsSection = $("div.teacher-interviews").eq(0)
+
+    let firstNavLink = $(".nav-link").eq(0)
+    let secondNavLink = $(".nav-link").eq(1)
+    let thirdNavLink = $(".nav-link").eq(2)
+    let fourthNavLink = $(".nav-link").eq(3)
+    let fifthNavLink = $(".nav-link").eq(4)
+    let sixthNavLink = $(".nav-link").eq(5)
+
+    let type_current_user = studentsDefaultView
 
     // Manage buttons click
     $("#studentsMessages").on("click", function () { showCurrentSection(studentsMessagesSection) })
     $("#studentsRegister").on("click", function () { showCurrentSection(studentsRegisterSection) })
     $("#studentsMarks").on("click", function () { showCurrentSection(studentsMarksSection) })
     $("#studentsAbsences").on("click", function () { showCurrentSection(studentsAbsencesSection) })
-    $("#studentsInformations").on("click", function () { showCurrentSection(studentsInformations) })
+    $("#personalInformations").on("click", function () { showCurrentSection(personalInformations) })
     $("#schoolReport").on("click", function () { showCurrentSection(studentsSchoolReport) })
 
-    $(".nav-link").eq(0).on("click", function () { showCurrentSection(studentsMessagesSection) })
-    $(".nav-link").eq(1).on("click", function () { showCurrentSection(studentsRegisterSection) })
-    $(".nav-link").eq(2).on("click", function () { showCurrentSection(studentsMarksSection) })
-    $(".nav-link").eq(3).on("click", function () { showCurrentSection(studentsAbsencesSection) })
-    $(".nav-link").eq(4).on("click", function () { showCurrentSection(studentsInformations) })
-    $(".nav-link").eq(5).on("click", function () { showCurrentSection(studentsSchoolReport) })
+    $("#teachersMessages").on("click", function () { showCurrentSection(teachersMessagesSection) })
+    $("#teachersRegister").on("click", function () { showCurrentSection(teachersRegisterSection) })
+    $("#teachersList").on("click", function () { showCurrentSection(teachersClassListSection) })
+    $("#teachersInterviews").on("click", function () { showCurrentSection(teachersInterviewsSection) })
+    $("#averages").on("click", function () { showCurrentSection(teachersAveragesSection) })
 
     sendRequest("GET", "php/studente.php").catch(error).then(function (response) {
         let user_data = response["data"]
@@ -40,29 +58,59 @@ window.onload = function () {
         // Personal informations
         $("#nominative").text(nominative)
         $("#residence").text(`${user_data["residenza"]} (${user_data["indrizzo"]})`)
-        // Navbar management
-        NavbarManagement()
+
+        fourthNavLink.on("click", function () { showCurrentSection(personalInformations) })
 
         if (user_data["docente"] == 0) {
             studentsDefaultView.show()
             $("#classroom").text(user_data["classe"])
             $("#role").text("Studente")
+            // Manage navbar links
+            firstNavLink.on("click", function () { showCurrentSection(studentsMessagesSection) })
+            secondNavLink.on("click", function () { showCurrentSection(studentsRegisterSection) })
+            thirdNavLink.on("click", function () { showCurrentSection(studentsMarksSection) })
+            fifthNavLink.on("click", function () { showCurrentSection(studentsAbsencesSection) })
+            sixthNavLink.text("Pagella").on("click", function () { showCurrentSection(studentsSchoolReport) })
         } else {
-            teachersSection.show()
+            teachersDefaultView.show()
+            // Set the type of the current user (teacher or student)
+            type_current_user = teachersDefaultView
+            // Disable teachers buttons
+            buttonsEnabled(false)
+            // Get all classes
             sendRequest("GET", "php/classi.php").catch(error).then(function (response) {
-                // Get all classes
                 let classes = []
-                for (let _class of response["data"])
+                for (let _class of response["data"]) {
                     classes.push(_class["nome"])
+                    $("<a>").addClass("dropdown-item").text(_class["nome"]).appendTo($(".dropdown-menu.classes").eq(0)).on("click", function () {
+                        let current_class = $(this).text()
+                        // Change button text
+                        $("a.dropdown-toggle.class").text(current_class)
+                        // FARE LOGIN NELLA CLASSE CORRENTE
+                        buttonsEnabled(true)
+                    })
+                }
                 // Load personal infos
                 $("#classroom").text(classes)
                 $("#role").text("Docente")
+                // Manage navbar links
+                firstNavLink.on("click", function () { showCurrentSection(teachersMessagesSection) })
+                secondNavLink.on("click", function () { showCurrentSection(teachersRegisterSection) })
+                thirdNavLink.on("click", function () { showCurrentSection(teachersMarksSection) })
+                fifthNavLink.on("click", function () { showCurrentSection(teachersAbsencesSection) })
+                sixthNavLink.text("Media").on("click", function () { showCurrentSection(teachersAveragesSection) })
             })
         }
+        // Navbar management
+        NavbarManagement()
     })
 
+    function buttonsEnabled(flag) {
+        $(".teacher-options button.btn").prop("disabled", !flag)
+    }
+
     function NavbarManagement() {
-        aProfile.on("click", showPersonalInformations)
+        aProfile.on("click", function () { showCurrentSection(personalInformations) })
 
         aExit.on("click", function () {
             sendRequest("POST", "php/logout.php").catch(error).then(function () {
@@ -70,14 +118,7 @@ window.onload = function () {
             })
         })
 
-        $(".navbar-brand").eq(0).on("click", function () { showCurrentSection(studentsDefaultView) })
-    }
-
-    function showPersonalInformations() {
-        studentsInformations.show()
-        studentsRegisterSection.hide()
-        studentsDefaultView.hide()
-        studentsMessagesSection.hide()
+        $(".navbar-brand").eq(0).on("click", function () { showCurrentSection(type_current_user) })
     }
 
     function showCurrentSection(_section) {
